3813a325a582d59a69443c07a22977e06ce475f6
coder-supratim <supratim.arya@gmail.com>
2020-04-25 20:23:20 -0400

    added some figlet font logger

--- src/main/java/com/timsoft/deviceregistrar/app/ServiceDeviceRegistrarApplication.java
+++ src/main/java/com/timsoft/deviceregistrar/app/ServiceDeviceRegistrarApplication.java
@@ -1,14 +1,16 @@
 package com.timsoft.deviceregistrar.app;
 
+import com.github.lalyos.jfiglet.FigletFont;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@Slf4j
 @SpringBootApplication(scanBasePackages = {"com.timsoft.deviceregistrar"})
 public class ServiceDeviceRegistrarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceDeviceRegistrarApplication.class, args);
 	}
-
+log.info(FigletFont.convertOneLine("Device Registrar"));
 }
 
