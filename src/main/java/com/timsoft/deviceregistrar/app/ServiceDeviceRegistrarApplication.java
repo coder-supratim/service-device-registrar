b46c7822f332841c25642ced0d8d4484347ff60d
coder-supratim <supratim.arya@gmail.com>
2020-04-27 02:06:40 -0400

    Added distributed tracing thru Zipkin

--- src/main/java/com/timsoft/deviceregistrar/app/ServiceDeviceRegistrarApplication.java
+++ src/main/java/com/timsoft/deviceregistrar/app/ServiceDeviceRegistrarApplication.java
@@ -4,13 +4,15 @@ import com.github.lalyos.jfiglet.FigletFont;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
 @Slf4j
 @SpringBootApplication(scanBasePackages = {"com.timsoft.deviceregistrar"})
 public class ServiceDeviceRegistrarApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(ServiceDeviceRegistrarApplication.class, args);
+		log.info(FigletFont.convertOneLine("Device Registrar"));
 	}
-log.info(FigletFont.convertOneLine("Device Registrar"));
 }
-
